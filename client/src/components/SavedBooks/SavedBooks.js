f93bec7a0bbead812e3a4a505565c5c02e8839cc
Mike Slaby <mikeslaby3@gmail.com>
2019-08-05 16:34:57 -0700

    Set up basic class component for saved books

---
+++ client/src/components/SavedBooks/SavedBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "../../App.css";
+
+class SavedBooks extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container component-container">
+                    <h1 className="section-title has-text-info">Saved Books</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SavedBooks;
