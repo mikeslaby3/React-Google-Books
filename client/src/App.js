41229b00cfc79f4ee32f2946092f51580c391e7b
Mike Slaby <mikeslaby3@gmail.com>
2019-08-04 19:29:46 -0700

    Connected routes for existing pages and cleared default app css

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
+import Saved from "./pages/Saved/Saved";
+import Nav from "./components/Nav/Nav";
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
