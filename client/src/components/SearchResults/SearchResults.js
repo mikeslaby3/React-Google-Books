71a70468aa1a6eb3fbaf9fd66dcc4de33373100c
Mike Slaby <mikeslaby3@gmail.com>
2019-08-05 16:37:05 -0700

    Created container for search results

---
+++ client/src/components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "../../App.css";
+
+class SearchResults extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container component-container">
+                    <h1 className="section-title has-text-info">Book Results</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchResults;
