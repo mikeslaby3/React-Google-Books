1b7728f74a3cd59a0c4cb3774ad50c5a4a04ee62
Mike Slaby <mikeslaby3@gmail.com>
2019-08-05 16:38:10 -0700

    Added saved books component to saved page

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SavedBooks from "../../components/SavedBooks/SavedBooks";
 
 class Saved extends Component {
     render() {
         return (
             <div>
-                
+                <SavedBooks />
             </div>
         )
     }
