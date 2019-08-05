4fec28315efb0b529337049fe6cca385c99fa32e
Mike Slaby <mikeslaby3@gmail.com>
2019-08-05 16:37:45 -0700

    Added search bar and results componennts to search

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from "../../components/SearchBar/SearchBar";
+import SearchResults from "../../components/SearchResults/SearchResults";
 
 class Search extends Component {
     render() {
         return (
             <div>
-                
+                <SearchBar />
+                <SearchResults />
             </div>
         )
     }
