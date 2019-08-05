31952f0856bf6a1952fec7d648179858d5ede4ee
Mike Slaby <mikeslaby3@gmail.com>
2019-08-05 16:33:17 -0700

    Set up hero banner for all pages

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Jumbotron = () => {
-    return (
-        <div>
-            
+
+  const owl = {
+    height: "150px"
+  }
+
+  return (
+    <div>
+      <section className="hero is-small is-info is-bold">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">React Google Books Search</h1>
+            <h2 className="subtitle is-size-3">Search for and Save Books of Interest</h2>
+            <img
+                style={owl}
+                src={process.env.PUBLIC_URL + './images/book-owl.png'}
+                alt="book-owl"/>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default Jumbotron;
