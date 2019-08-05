4bd0f0fefb7228a6e20170fd67521ac23f5497d2
Mike Slaby <mikeslaby3@gmail.com>
2019-08-04 19:34:07 -0700

    Basic server set up

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8050;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
