93bd3f0bba6958e0986b08e88ac531e9f722f299
Mike Slaby <mikeslaby3@gmail.com>
2019-08-06 16:29:42 -0700

    Added new URL parser

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const path = require("path");
+
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8050;
 const app = express();
+const routes = require('./routes');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+    {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,10 +21,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes);
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
