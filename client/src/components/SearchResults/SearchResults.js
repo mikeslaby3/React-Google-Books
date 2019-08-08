dc2656455e65bed31599e9893fc6c521a146bb0a
Mike Slaby <mikeslaby3@gmail.com>
2019-08-08 14:26:31 -0700

    Chnaged from class to functional component

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Book from "../Book/Book";
 import "../../App.css";
 
-class SearchResults extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container component-container">
-                    <h1 className="section-title has-text-info">Book Results</h1>
-                </div>
-            </div>
-        )
+const SearchResults = (props) => {
+    const messageStyling = {
+        marginBottom: "20px",
+        fontSize: "18px"
     }
+    return (
+        <div>
+            <div className="container component-container">
+                <h1 className="section-title has-text-info">Book Results</h1>
+                <h2 style={messageStyling}>{props.message}</h2>
+                <Book />
+            </div>
+        </div>
+    )
 }
 
 export default SearchResults;
